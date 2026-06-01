32125e081a25aeb7aa1e1e8150e713a79d69c105
albert123-arch <iskhakovalbert1@gmail.com>
2026-06-01 17:54:16 +0500

    Harden Hostinger startup entrypoint

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-process.env.HOSTNAME = process.env.HOSTNAME || "0.0.0.0";
-process.env.PORT = process.env.PORT || "3000";
+process.env.HOSTNAME = "0.0.0.0";
+process.env.PORT = "3000";
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught server exception", error);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.error("Unhandled server rejection", error);
+});
+
+console.log("Starting Maths4U Arena on 0.0.0.0:3000");
 
 void import("./.next/standalone/server.js");
